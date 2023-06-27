eabb1c3ba03d113304b7ff4ab9f8e1e0f236c966
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-27 23:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var audio = new Audio();
+function playSound(filename) {
+	audio.src = 'sounds/' + filename;
+    audio.play();
+}
+function stopSound() {
+	audio.pause();
+    audio.currentTime = 0;
+}
